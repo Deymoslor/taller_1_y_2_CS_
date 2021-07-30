6846c81175ccc430f8df186d78fc4d99f0cbdd23
Deymoslor <dylanmurillo.rios.inead@gmail.com>
2021-07-30 14:17:16 -0500

    proyectos para el taller1

---
+++ taller_1/ejercicio22/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ejercicio22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ taller_1/ejercicio24/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ejercicio24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
