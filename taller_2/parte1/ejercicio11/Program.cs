fae571ace3e8b3b50781868b6959d970d77fe525
unknown <kesan0309@gmail.com>
2021-08-01 14:07:12 -0500

    punto 11, parte 1

--- taller_2/parte1/ejercicio11/Program.cs
+++ taller_2/parte1/ejercicio11/Program.cs
@@ -6,7 +6,25 @@ namespace ejercicio11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
+            Console.WriteLine("Ingrese la nota 1: ");
+            double b1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la nota 2: ");
+            double b2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la nota 3: ");
+            double b3 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la nota 4: ");
+            double b4 = double.Parse(Console.ReadLine());
+            double pro = ((b1 + b2 + b3 + b4) / 4);
+            if (pro < 3.5)
+            {
+                Console.WriteLine("Reprobò");
+            }
+            else
+            {
+                Console.WriteLine("Aprobò");
+            }
+            Console.WriteLine("");
         }
     }
 }
