f5b3d31d5c1f4508e803c2c9de3178d94221d061
OscarAR <oscarandresrolonmeneses@gmail.com>
2021-07-31 19:14:19 -0500

    ejercicio 10

--- taller_1/ejercicio10/Program.cs
+++ taller_1/ejercicio10/Program.cs
@@ -6,7 +6,14 @@ namespace ejercicio10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            decimal valor, total,descuento;
+            Console.Write("Ingrese el valor de la compra: ");
+            valor = decimal.Parse(Console.ReadLine());
+            descuento = valor * 0.15m;
+            total = valor - descuento;
+
+            Console.Write($"\nEl valor de su compra es de ${valor.ToString("N0")}, el cual tiene un descuento de ${descuento.ToString("N0")}; lo cual da un valor a pagar de ${total.ToString("N0")}.\n");
+
         }
     }
 }
