867d3f09c8e7a3f09c19e0e00c45d687a37add28
OscarAR <oscarandresrolonmeneses@gmail.com>
2021-07-31 19:30:46 -0500

    Parte 1, Punto 14

--- taller_2/parte1/ejercicio14/Program.cs
+++ taller_2/parte1/ejercicio14/Program.cs
@@ -6,7 +6,15 @@ namespace ejercicio14
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1, num2;
+            Console.Write("Ingrese numero 1: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese numero 2: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+                Console.Write($"\nEl Primer numero ingresado es: {num1}\nEl segungo numero es: {num2}\n\nLa suma de los dos es: {num1 + num2}\n");
         }
     }
 }
