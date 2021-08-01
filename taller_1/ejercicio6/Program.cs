b47ab6228907d24752228532305a91dc580f33bb
OscarAR <oscarandresrolonmeneses@gmail.com>
2021-07-31 19:12:02 -0500

    Punto  6

--- taller_1/ejercicio6/Program.cs
+++ taller_1/ejercicio6/Program.cs
@@ -6,7 +6,33 @@ namespace ejercicio6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            static void Main(string[] args)
+        {
+            decimal valor,iva,total,descuento,val;
+            String respuesta;
+
+            Console.Write("Ingrese el valor de la compra: ");
+            valor = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Autoriza el descuento: ");
+            respuesta = Console.ReadLine();
+
+            if ((respuesta == "SI")||(respuesta == "Si")|| (respuesta == "si"))
+            {
+                descuento = valor*0.10m;
+                val = valor - descuento;
+                iva = val * 0.19m;
+                total = val + iva;
+
+                Console.Write($"\nEl valor de la compra es de: ${valor.ToString("N0")}, al cual se le autorizó un descuento de ${descuento.ToString("N0")}, el Iva de este producto es de: {iva.ToString("N0")}, lo cual da para un valor a pagar de ${total.ToString("N0")}.");
+            }
+            else
+            {
+                iva = valor * 0.19m;
+                total = valor + iva;
+
+                Console.Write($"\nEl valor de la compra es de: ${valor.ToString("N0")}, el cual tiene un iva de ${iva.ToString("N0")}; para un total a pagar de  ${total.ToString("N0")}");
+            }
         }
     }
 }
