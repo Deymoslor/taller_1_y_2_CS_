003d13a907065152e6f90684c093ceafd2957cb3
unknown <kesan0309@gmail.com>
2021-08-01 13:52:44 -0500

    punto 23

--- taller_1/ejercicio23/Program.cs
+++ taller_1/ejercicio23/Program.cs
@@ -6,7 +6,12 @@ namespace ejercicio23
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
+            Console.WriteLine("Ingrese la cantidad de metros que decea subir: ");
+            float mm = float.Parse(Console.ReadLine());
+            float hh = (float)0.714285714286;
+            float mh = mm * hh;
+            Console.WriteLine("El tiempo que tardarà es: " + mh + " horas");
         }
     }
 }
