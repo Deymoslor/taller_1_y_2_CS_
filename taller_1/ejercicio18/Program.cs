761af77c765713c70ebdafcae1815e2026397991
OscarAR <oscarandresrolonmeneses@gmail.com>
2021-07-31 19:18:53 -0500

    Punto 18

--- taller_1/ejercicio18/Program.cs
+++ taller_1/ejercicio18/Program.cs
@@ -4,9 +4,19 @@ namespace ejercicio18
 {
     class Program
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            decimal valor, primera, segunda, tercera, cuarta;
+
+            Console.Write("Ingrese el valor de la mátricula: ");
+            valor = decimal.Parse(Console.ReadLine());
+
+            primera = valor * 0.40m;
+            segunda = valor * 0.25m;
+            tercera = valor * 0.20m;
+            cuarta = valor * 0.15m;
+
+            Console.Write($"\nLas cuotas que deben pagar el estudiante son:\n  - Primera cuota (40%): ${primera.ToString("N0")}\n  - segunda cuota (25%): ${segunda.ToString("N0")}\n  - Tercera cuota (20%): ${tercera.ToString("N0")}\n  - Cuarta cuota (15%): ${cuarta.ToString("N0")}\n  **El valor a pagar es de ${valor.ToString("N0")}**");
         }
     }
 }
