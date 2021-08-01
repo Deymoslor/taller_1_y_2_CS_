4f91364d987649c35e26050f539142c91127d3c0
unknown <kesan0309@gmail.com>
2021-08-01 14:10:30 -0500

    punto 15, parte 1

--- taller_2/parte1/ejercicio15/Program.cs
+++ taller_2/parte1/ejercicio15/Program.cs
@@ -6,7 +6,16 @@ namespace ejercicio15
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
+            Console.WriteLine("Ingrese un numero: ");
+            int cubo = 0;
+            int c = int.Parse(Console.ReadLine());
+            if (c >= 10)
+            {
+                cubo = c * c * c;
+                Console.WriteLine("");
+                Console.WriteLine("El cubo es: " + cubo);
+            }
         }
     }
 }
