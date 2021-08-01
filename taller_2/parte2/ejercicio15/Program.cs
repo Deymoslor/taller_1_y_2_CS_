30ff7bc9ebd84eaee4523568cafc3c967548dc14
OscarAR <oscarandresrolonmeneses@gmail.com>
2021-07-31 19:39:01 -0500

    Parte 2, Punto 15

--- taller_2/parte2/ejercicio15/Program.cs
+++ taller_2/parte2/ejercicio15/Program.cs
@@ -6,7 +6,15 @@ namespace ejercicio15
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            decimal valor;
+
+            Console.Write("Ingrese valor de comprea: ");
+            valor = decimal.Parse(Console.ReadLine());
+
+            if (valor >= 200000)
+                Console.Write($"\nEl descuento de su compra es de : ${(valor *0.17m).ToString("N0")} para un total de ${(valor-(valor * 0.17m)).ToString("N0")}");
+            else
+                Console.Write($"\nEl descuento de su compra es de : ${(valor * 0.05m).ToString("N0")} para un total de ${(valor - (valor * 0.17m)).ToString("N0")}");
         }
     }
 }
