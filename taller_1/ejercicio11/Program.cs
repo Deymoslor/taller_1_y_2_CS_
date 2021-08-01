741f5c0cb339fb2fff6e78e08963b066eb6a5b79
unknown <kesan0309@gmail.com>
2021-08-01 13:39:06 -0500

    punto 11

--- taller_1/ejercicio11/Program.cs
+++ taller_1/ejercicio11/Program.cs
@@ -6,7 +6,22 @@ namespace ejercicio11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
+            Console.WriteLine("Ingrese su nota parcial: ");
+            float nota1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su nota parcial: ");
+            float nota2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su nota parcial: ");
+            float nota3 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la nota del examen final: ");
+            float ef = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la nota del trabajo final: ");
+            float tf = float.Parse(Console.ReadLine());
+            double npf = ((nota1 + nota2 + nota3) / 3) * 0.55;
+            double etf = ef * 0.3;
+            double ttf = tf * 0.15;
+            Console.WriteLine("Su nota final es: " + (npf + etf + ttf));
+            Console.WriteLine("");
         }
     }
 }
