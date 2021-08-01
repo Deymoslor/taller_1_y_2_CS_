a96ed25771753160c6f95263f734110cac3c1c7c
unknown <kesan0309@gmail.com>
2021-08-01 13:59:20 -0500

    punto 3, parte 1

--- taller_2/parte1/ejercicio3/Program.cs
+++ taller_2/parte1/ejercicio3/Program.cs
@@ -6,7 +6,18 @@ namespace ejercicio3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
+            Console.WriteLine("Ingrese el precio de la computadora que desea comprar: ");
+            double p = double.Parse(Console.ReadLine());
+            if (p > 1000000.0)
+            {
+                p = p - (p * 0.1);
+                Console.WriteLine("Descuento del 10%: " + p);
+            }
+            Console.WriteLine("");
+            p = p + (p * 0.19);
+            Console.WriteLine("Precio TOTAL con IVA incluido: ");
+            Console.WriteLine("");
         }
     }
 }
