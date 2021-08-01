fce9b360fe74482893771983dc012b9d2ab52bbd
unknown <kesan0309@gmail.com>
2021-08-01 14:16:50 -0500

    punto 8, parte 2

--- taller_2/parte2/ejercicio8/Program.cs
+++ taller_2/parte2/ejercicio8/Program.cs
@@ -6,7 +6,19 @@ namespace ejercicio8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double pull = 0;
+            Console.WriteLine("Ingrese su edad: ");
+            int edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su sexo (Si es mujer ingrese 1 y si es hombre ingrese 2)");
+            int sexo = int.Parse(Console.ReadLine());
+            if (sexo == 1)
+            {
+                pull = (220-edad)/10;
+            }else if (sexo == 2)
+            {
+                pull = (210-edad)/10;
+            }
+            Console.WriteLine("La cantidad e pulsaciones que deberìas tener es: ",pull);
         }
     }
 }
