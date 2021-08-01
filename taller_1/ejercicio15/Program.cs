56a60df562d4dfb38e4abf2719f65aaab32315d5
unknown <kesan0309@gmail.com>
2021-08-01 13:44:50 -0500

    punto 15

--- taller_1/ejercicio15/Program.cs
+++ taller_1/ejercicio15/Program.cs
@@ -6,7 +6,25 @@ namespace ejercicio15
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
+            double tt = 0, tta = 0;
+            String pregunta;
+            do
+            {
+                Console.WriteLine("Ingrese el precio del producto: ");
+                tt = double.Parse(Console.ReadLine());
+                Console.WriteLine("¿Quieres volver a ingresar? R// ");
+                pregunta = Console.ReadLine();
+                tta = tta + tt;
+            } while (pregunta.Equals("si"));
+            tta = (tta * 0.19) + tta;
+            Console.WriteLine("");
+            Console.WriteLine("El total a pagar: " + tta);
+            Console.WriteLine("");
+            Console.WriteLine("Ingrese el dinero en efectivo: ");
+            double r = double.Parse(Console.ReadLine());
+            Console.WriteLine("Su cambio es: " + (tta - r));
+            Console.WriteLine("");
         }
     }
 }
