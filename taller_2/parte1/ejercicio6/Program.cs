f67e584c485d797a31dfe4a1ffda966bf8061571
OscarAR <oscarandresrolonmeneses@gmail.com>
2021-07-31 19:27:24 -0500

    Parte 1, Punto6

--- taller_2/parte1/ejercicio6/Program.cs
+++ taller_2/parte1/ejercicio6/Program.cs
@@ -6,7 +6,23 @@ namespace ejercicio6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            decimal salarioBruto, salario, retencion;
+
+            Console.Write("Ingrese el salario del empleado: ");
+            salarioBruto = decimal.Parse(Console.ReadLine());
+
+            if (salarioBruto > 2000000)
+            {
+                retencion = salarioBruto * 0.10m;
+                salario = salarioBruto - retencion;
+
+                Console.Write($"\nSu salario bruto es de ${salarioBruto.ToString("N0")} , el cual supera los $2.000.000, por lo cual tiene una retención de ${retencion.ToString("N0")}.\nSu salario final es ${salario.ToString("N0")}.\n");
+            }
+            else
+            {
+                salario = salarioBruto;
+                Console.WriteLine($"Su salario es ${salario.ToString("N0")}");
+            }
         }
     }
 }
