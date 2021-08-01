6476cd2c6bfaa0aa2c3bd61e9fe1cfb15ae98334
unknown <kesan0309@gmail.com>
2021-08-01 14:20:02 -0500

    punto 12, parte 2

--- taller_2/parte2/ejercicio12/Program.cs
+++ taller_2/parte2/ejercicio12/Program.cs
@@ -6,7 +6,17 @@ namespace ejercicio12
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ingrese la cantidad de cuadernos que desea comprar");
+            int cuadernos = int.Parse(Console.ReadLine());
+            if (cuadernos >= 5)
+            {
+                Console.WriteLine("Cada cuaderno vale 2,000$");
+                Console.Write($"\nEn total debera pagar: {(cuadernos*2000)}.\n");
+            }else
+            {
+                Console.WriteLine("Cada cuaderno vale 2,500$");
+                Console.Write($"\nEn total debera pagar: {(cuadernos*2500)}.\n");
+            }
         }
     }
 }
