c2046f79ad2ed809744bb54f9dec39c5d69914c8
unknown <kesan0309@gmail.com>
2021-08-01 13:31:08 -0500

    punto 3

--- taller_1/ejercicio3/Program.cs
+++ taller_1/ejercicio3/Program.cs
@@ -6,7 +6,16 @@ namespace ejercicio3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
+            Console.WriteLine("Ingrese su nota: ");
+            float n1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su nota: ");
+            float n2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su nota: ");
+            float n3 = float.Parse(Console.ReadLine());
+            float nt = (n1 + n2 + n3) / 3;
+            Console.WriteLine("La nota definitiva es: " + nt);
+            Console.WriteLine("");
         }
     }
 }
