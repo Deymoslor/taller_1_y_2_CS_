3c97f0cceab74aaf24c49e5d6164848e658b43d6
OscarAR <oscarandresrolonmeneses@gmail.com>
2021-07-31 19:25:17 -0500

    Parte 1, Punto2

--- taller_2/parte1/ejercicio2/Program.cs
+++ taller_2/parte1/ejercicio2/Program.cs
@@ -6,7 +6,21 @@ namespace ejercicio2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num;
+            Console.Write("Ingrese numero: ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num >= 10)
+            {
+                int triple = num * 3;
+                Console.WriteLine($"El triple del numero ingresado es: {triple}");
+            }
+            else
+            {
+                Console.WriteLine($"El numero ingresado es: {num}");
+            }
+                
+
         }
     }
 }
