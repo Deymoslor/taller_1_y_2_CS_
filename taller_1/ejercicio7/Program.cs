5982768b6ae7a1b28586a9ad71f01c4f5ab7a0a1
unknown <kesan0309@gmail.com>
2021-08-01 13:35:33 -0500

    punto 7

--- taller_1/ejercicio7/Program.cs
+++ taller_1/ejercicio7/Program.cs
@@ -6,7 +6,20 @@ namespace ejercicio7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
+            int hora, minuto, segundo;
+            Console.WriteLine("Ingrese la hora: ");
+            hora = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese los minutos: ");
+            minuto = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese los segundos: ");
+            segundo = int.Parse(Console.ReadLine());
+            hora = hora * 60;
+            minuto = (minuto * 60) + (hora * 60);
+            segundo = segundo + minuto;
+            Console.WriteLine("");
+            Console.WriteLine("El total de segundos son: " + segundo + "s");
+            Console.WriteLine("");
         }
     }
 }
